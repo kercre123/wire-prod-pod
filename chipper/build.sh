29313a92d916cdf93d96da24ac1dafbf8215456d
Wire <whyyoulittle333@gmail.com>
2022-11-05 23:02:28 -0500

    Add vosk to build.sh

--- chipper/build.sh
+++ chipper/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-	export CGO_LDFLAGS="-L/root/.coqui/"
-	export CGO_CXXFLAGS="-I/root/.coqui/"
-	export LD_LIBRARY_PATH="/root/.coqui/:$LD_LIBRARY_PATH"
+  	export CGO_CXXFLAGS="-I/root/.coqui/"
+	export CGO_ENABLED=1
+	export CGO_CFLAGS="-I/root/.vosk/libvosk"
+	export CGO_LDFLAGS="-L/root/.coqui/ -L /root/.vosk/libvosk -lvosk -ldl -lpthread"
+	export LD_LIBRARY_PATH="/root/.coqui/:/root/.vosk/libvosk:$LD_LIBRARY_PATH"
 UNAME=$(uname -a)
 echo "Building chipper..."
 /usr/local/go/bin/go build cmd/main.go
